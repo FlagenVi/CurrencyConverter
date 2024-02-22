bb744319442869b305f1a3608ece667e323d43a3
afoni <afoninvtialya3@gmail.com>
2024-02-22 13:25:07 +0300

    Финальный результат думаю, всякие условия не добавлял, поэтому если выбивать будет, то увы

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,14 @@ import org.xml.sax.InputSource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import java.io.IOException;
 import java.io.StringReader;
+
+//Библиотека для ввода данных в консоль
 import java.util.Scanner;
 
 public class Main {
@@ -21,8 +25,9 @@ public class Main {
 
 
         GetExample example = new GetExample();
-        String response = example.run("http://www.cbr.ru/scripts/XML_daily.asp?date_req=22/02/2024");
+        String response = example.run("http://www.cbr.ru/scripts/XML_daily.asp?date_req=23/02/2024");
         try {
+
             //Для чтения XML
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -34,20 +39,28 @@ public class Main {
             {
                 Element valute = (Element) valuteList.item(i);
                 String name = valute.getElementsByTagName("Name").item(0).getTextContent();
-                String value = valute.getElementsByTagName("Value").item(0).getTextContent();
-                System.out.println((i+1) + ". " + name + ", Курс: " + value);
+                String value = valute.getElementsByTagName("VunitRate").item(0).getTextContent();
+                System.out.println((i+1) + ". " + name + ", Курс за 1 единицу: " + value);
             }
+            System.out.println("Выберите из перечисленных валюту, в которую вы хотите конвертировать рубль:");
+            int num = in.nextInt();
+
+            Element chosenValute = (Element) valuteList.item(num - 1);
+            String chosenCharCode = chosenValute.getElementsByTagName("CharCode").item(0).getTextContent();
+            String chosenName = chosenValute.getElementsByTagName("Name").item(0).getTextContent();
+            String chosenValue = chosenValute.getElementsByTagName("VunitRate").item(0).getTextContent();
+
+            System.out.println("Вы выбрали валюту: " + chosenCharCode + " - " + chosenName);
+            System.out.println("Текущий курс: " + chosenValue);
+
+            System.out.print("Введите количество рублей для конвертации: ");
+            float rubAmount = in.nextFloat();
+
+            System.out.println("Результат: " + rubAmount * Float.parseFloat(chosenValue.replace(',','.')));
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
-        System.out.println("Выберите из перечисленных валюту, в которую вы хотите конвертировать рубль:");
-        int num = in.nextInt();
-        System.out.println("Введите количество рублей, что у вас есть:");
-        float fnum = in.nextFloat();
-
-
     }
 }
